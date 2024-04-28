e54e2f837c11a14203619116c024c01a56f87552
shuding0307 <suhyun2116@gmail.com>
2024-04-28 20:41:40 +0900

    fix input size

--- src/app/test/_component/InputInfoPage.tsx
+++ src/app/test/_component/InputInfoPage.tsx
@@ -9,13 +9,13 @@ export default function InputInfoPage({goNext} : {goNext : () => void}) {
       <div id="container" className="flex flex-col items-center justify-between border-2 border-gray-300 rounded-xl py-8 w-[500px] bg-yellow-50 h-[494px] mt-20 sm:mt-36 sm:w-72 sm:h-3/5">
       <h2 className='text-xl'>정신연령 테스트</h2>
         <form className="flex flex-col gap-3">
-          <input className="bg-white-50 text-blue-gray-700 border focus:border-1 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 "
+          <input className="bg-white text-blue-gray-700 border focus:border-1 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 sm:w-1/2"
                 type="text" 
                 id="name"
                 placeholder="이름"
                 onChange={(e) => setUserName(e.target.value)}>
           </input>
-          <input className="bg-white-50 text-blue-gray-700 border focus:border-1 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 "
+          <input className="bg-white text-blue-gray-700 border focus:border-1 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 sm:w-1/2"
                 type="number" 
                 id="age"
                 placeholder="나이"
