f7e640635945af52e926b9f7ea937e64ed65ae39
shuding0307 <suhyun2116@gmail.com>
2024-04-28 20:43:45 +0900

    Update InputInfoPage.tsx

--- src/app/test/_component/InputInfoPage.tsx
+++ src/app/test/_component/InputInfoPage.tsx
@@ -8,7 +8,7 @@ export default function InputInfoPage({goNext} : {goNext : () => void}) {
     <div className='flex justify-center sm:w-full sm:h-screen'>
       <div id="container" className="flex flex-col items-center justify-between border-2 border-gray-300 rounded-xl py-8 w-[500px] bg-yellow-50 h-[494px] mt-20 sm:mt-36 sm:w-72 sm:h-3/5">
       <h2 className='text-xl'>정신연령 테스트</h2>
-        <form className="flex flex-col gap-3">
+        <form className="flex flex-col items-center gap-3">
           <input className="bg-white text-blue-gray-700 border focus:border-1 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 sm:w-1/2"
                 type="text" 
                 id="name"
