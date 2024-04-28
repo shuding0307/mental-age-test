c9bd269145112ce6befb69b0635ee5c828563bb8
shuding0307 <suhyun2116@gmail.com>
2024-04-28 21:42:22 +0900

    Update KakaoShare.tsx

---
+++ src/Components/KakaoShare.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+import KakaoImage from "/public/Kakao_logo.jpg";
+
+type KakaoShareButtonProps = {
+  description: string;
+};
+
+const KakaoShareButton = ({ description }: KakaoShareButtonProps) => {
+  const shareUrl = typeof window !== "undefined" ? window.location.href : "https://mental-age-test.vercel.app";
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const { Kakao } = window;
+
+      if (!Kakao.isInitialized()) {
+        Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+      }
+    }
+  }, []);
+
+  const handleShare = () => {
+    const { Kakao } = window;
+
+    Kakao.Share.sendDefault({
+      objectType: "text",
+      text: description,
+      link: {
+        mobileWebUrl: shareUrl,
+        webUrl: shareUrl,
+      },
+    });
+  };
+
+  return (
+    <div onClick={handleShare}>
+      <Image
+        className="w-10 h-10 cursor-pointer"
+        src={KakaoImage}
+        alt="카카오톡 공유 이미지"
+      />
+    </div>
+  );
+};
+
+export default KakaoShareButton;
